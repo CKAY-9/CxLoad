7c50a54ea7cef4bf48713a95e71715ed88f5281b
Debuhg <foxtrotcyanide@gmail.com>
2023-05-03 21:10:54 -0700

    Fix errors.

--- cxloadaddon/lua/autorun/cxload_autorun.lua
+++ cxloadaddon/lua/autorun/cxload_autorun.lua
@@ -1,12 +1,26 @@
 if SERVER then
-    include("config.lua")
+    include("cxload_config.lua")
 
     hook.Add("PlayerChangedTeam", "PlayerChange", function(ply, old, new)
-        if type(ply:isArrested()) ~= nil then
+        if ply:isArrested() == nil then
             -- Gamemode is DarkRP derived
             http.Post(CXLOAD.url .. "/game/updatePlayer.php", {
                 steamid = ply:SteamID64(),
-                money = tostring(ply:getDarkRPVar("money"))
+                money = tostring(ply:getDarkRPVar("money")),
+            }, function()
+                if CXLOAD.logs then
+                    print("Updated " .. ply:GetName() .. " information on webhost")
+                end
+            end, function(message)
+                if CXLOAD.logs then
+                    print("Failed to update on webhost: " .. message)
+                end
+            end)
+        else
+            -- Gamemode is not DarkRP derived
+            http.Post(CXLOAD.url .. "/game/updatePlayer.php", {
+                steamid = ply:SteamID64(),
+                removeMoney = true,
             }, function()
                 if CXLOAD.logs then
                     print("Updated " .. ply:GetName() .. " information on webhost")
